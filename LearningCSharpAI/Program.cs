90525f9880fa4d28dd39ae904797b38cd13e7906
Wei Wang <62213594+weiw11@users.noreply.github.com>
2025-09-30 23:00:06 -0400

    feat: add support for OllamaSharp package and initialize configuration with model selection functionality

--- LearningCSharpAI/Program.cs
+++ LearningCSharpAI/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// set up the client
 
-Console.WriteLine("Hello, World!");
+using DotNetEnv;
+using OllamaSharp;
+
+Env.TraversePath().Load();
+
+var uri = new Uri(Env.GetString("OLLAMA_URI"));
+
+// Set to empty to pick from list at runtime
+var defaultModel = ""; 
+var ollama = new OllamaApiClient(uri: uri, defaultModel: defaultModel);
+
+if (string.IsNullOrEmpty(defaultModel))
+{
+    var models = (await ollama.ListLocalModelsAsync()).ToList();
+    Console.WriteLine("========== MODELS ==========");
+    foreach (var model in models)
+    {
+        Console.WriteLine(model.Name);
+    }
+    Console.WriteLine("========== END MODELS ==========");
+    Console.WriteLine();
+    
+    while (true)
+    {
+        Console.Write("Enter a model from list: ");
+        var userInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(userInput)) continue;
+
+        var model = models.Find(m => m.Name.StartsWith(userInput));
+        if (model == null) continue;
+
+        Console.WriteLine($"Selected Model: {model.Name}");
+        Console.WriteLine();
+        
+        ollama.SelectedModel = model.Name;
+        break;
+    }
+}
+
+var chat = new Chat(ollama);
+
+while (true)
+{
+    Console.Write("Chat > ");
+    var message = Console.ReadLine();
+    
+    if (string.IsNullOrEmpty(message)) continue;
+    if (message == "end") break;
+    
+    await foreach (var answerToken in chat.SendAsync(message))
+        Console.Write(answerToken);
+    Console.WriteLine();
+}
